abbf91aaf1a1759c9e6c04a5597fca3b1b907880
JoshuaLetessier <147531448+JoshuaLetessier@users.noreply.github.com>
2025-03-01 15:27:49 +0100

    Update EncryptedSaveDecorator.cs

--- Packages/com.faolline.savesystem.encryption/Runtime/EncryptedSaveDecorator.cs
+++ Packages/com.faolline.savesystem.encryption/Runtime/EncryptedSaveDecorator.cs
@@ -1,30 +1,37 @@
-public class EncryptedSaveDecorator<T> : ISaveSystem<T>
+namespace SaveSystem
 {
-    private readonly ISaveSystem<T> _baseSaveSystem;
-
-    public EncryptedSaveDecorator(ISaveSystem<T> baseSaveSystem)
+    namespace Encrypted
     {
-        _baseSaveSystem = baseSaveSystem;
-    }
 
-    public void Save(string key, T data)
-    {
-        string json = JsonConvert.SerializeObject(data);
-        string encryptedJson = EncryptionUtility.Encrypt(json);
-        _baseSaveSystem.Save(key, (T)(object)encryptedJson);
-    }
-
-    public T Load(string key)
-    {
-        string encryptedJson = (string)(object)_baseSaveSystem.Load(key);
-        if (string.IsNullOrEmpty(encryptedJson)) return default;
-
-        string decryptedJson = EncryptionUtility.Decrypt(encryptedJson);
-        return JsonConvert.DeserializeObject<T>(decryptedJson);
-    }
-
-    public void Delete(string key)
-    {
-        _baseSaveSystem.Delete(key);
+        public class EncryptedSaveDecorator<T> : ISaveSystem<T>
+        {
+            private readonly ISaveSystem<T> _baseSaveSystem;
+        
+            public EncryptedSaveDecorator(ISaveSystem<T> baseSaveSystem)
+            {
+                _baseSaveSystem = baseSaveSystem;
+            }
+        
+            public void Save(string key, T data)
+            {
+                string json = JsonConvert.SerializeObject(data);
+                string encryptedJson = EncryptionUtility.Encrypt(json);
+                _baseSaveSystem.Save(key, (T)(object)encryptedJson);
+            }
+        
+            public T Load(string key)
+            {
+                string encryptedJson = (string)(object)_baseSaveSystem.Load(key);
+                if (string.IsNullOrEmpty(encryptedJson)) return default;
+        
+                string decryptedJson = EncryptionUtility.Decrypt(encryptedJson);
+                return JsonConvert.DeserializeObject<T>(decryptedJson);
+            }
+        
+            public void Delete(string key)
+            {
+                _baseSaveSystem.Delete(key);
+            }
+        }
     }
 }

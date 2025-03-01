945a57586dc7fb6073d86b4ee540e73cddb82a24
JoshuaLetessier <jletessier@gaming.tech>
2025-03-01 16:10:05 +0100

    make encruptionUtility

--- Packages/com.faolline.savesystem.encryption/Runtime/EncryptedSaveDecorator.cs
+++ Packages/com.faolline.savesystem.encryption/Runtime/EncryptedSaveDecorator.cs
@@ -6,16 +6,18 @@ namespace SaveSystem
         public class EncryptedSaveDecorator<T> : ISaveSystem<T>
         {
             private readonly ISaveSystem<T> _baseSaveSystem;
+            private readonly EncryptionUtility _encryptionUtility;
 
             public EncryptedSaveDecorator(ISaveSystem<T> baseSaveSystem)
             {
                 _baseSaveSystem = baseSaveSystem;
+                _encryptionUtility = new EncryptionUtility();
             }
 
             public void Save(string key, T data)
             {
                 string json = JsonConvert.SerializeObject(data);
-                string encryptedJson = EncryptionUtility.Encrypt(json);
+                string encryptedJson = _encryptionUtility.Encrypt(json);
                 _baseSaveSystem.Save(key, (T)(object)encryptedJson);
             }
 
@@ -24,7 +26,7 @@ namespace SaveSystem
                 string encryptedJson = (string)(object)_baseSaveSystem.Load(key);
                 if (string.IsNullOrEmpty(encryptedJson)) return default;
 
-                string decryptedJson = EncryptionUtility.Decrypt(encryptedJson);
+                string decryptedJson = _encryptionUtility.Decrypt(encryptedJson);
                 return JsonConvert.DeserializeObject<T>(decryptedJson);
             }
 
--- Packages/com.faolline.savesystem.encryption/Runtime/EncryptionUtility.cs
+++ Packages/com.faolline.savesystem.encryption/Runtime/EncryptionUtility.cs
@@ -1,30 +1,93 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 using UnityEngine;
-using Newtonsoft.Json;
 
-public class EncryptedPlayerPrefsSaveSystem<T> : ISaveSystem<T>
+public static class EncryptionUtility
 {
-    public void Save(string key, T data)
+    private static readonly string KeySavePath = Application.persistentDataPath + "/encryption_key.dat";
+
+    private static byte[] encryptionKey;
+
+    static EncryptionUtility()
     {
-        string json = JsonConvert.SerializeObject(data);
-        string encryptedJson = EncryptionUtility.Encrypt(json); // 🔒 Encrypt data
-        PlayerPrefs.SetString(key, encryptedJson);
-        PlayerPrefs.Save();
+        LoadOrGenerateKey();
     }
 
-    public T Load(string key)
+    private static void LoadOrGenerateKey()
     {
-        if (!PlayerPrefs.HasKey(key)) return default;
-
-        string encryptedJson = PlayerPrefs.GetString(key);
-        string decryptedJson = EncryptionUtility.Decrypt(encryptedJson); // 🔓 Decrypt data
-        return JsonConvert.DeserializeObject<T>(decryptedJson);
-    }
-
-    public void Delete(string key)
-    {
-        if (PlayerPrefs.HasKey(key))
+        if (File.Exists(KeySavePath))
         {
-            PlayerPrefs.DeleteKey(key);
+            encryptionKey = Convert.FromBase64String(File.ReadAllText(KeySavePath));
+        }
+        else
+        {
+            encryptionKey = GenerateSecureKey();
+            File.WriteAllText(KeySavePath, Convert.ToBase64String(encryptionKey));
+        }
+    }
+
+    private static byte[] GenerateSecureKey()
+    {
+        using (var rng = new RNGCryptoServiceProvider())
+        {
+            byte[] key = new byte[32]; // 256-bit key
+            rng.GetBytes(key);
+            return key;
+        }
+    }
+
+    public static string Encrypt(string plainText)
+    {
+        byte[] iv = new byte[16]; // IV de 16 bytes
+        using (var rng = new RNGCryptoServiceProvider())
+        {
+            rng.GetBytes(iv);
+        }
+
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = encryptionKey;
+            aesAlg.IV = iv;
+            aesAlg.Mode = CipherMode.CBC;
+            aesAlg.Padding = PaddingMode.PKCS7;
+
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                msEncrypt.Write(iv, 0, iv.Length); // Stocker l'IV au début
+
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, aesAlg.CreateEncryptor(), CryptoStreamMode.Write))
+                using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                {
+                    swEncrypt.Write(plainText);
+                }
+
+                return Convert.ToBase64String(msEncrypt.ToArray());
+            }
+        }
+    }
+
+    public static string Decrypt(string cipherText)
+    {
+        byte[] cipherBytes = Convert.FromBase64String(cipherText);
+
+        byte[] iv = new byte[16];
+        Array.Copy(cipherBytes, 0, iv, 0, iv.Length);
+
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = encryptionKey;
+            aesAlg.IV = iv;
+            aesAlg.Mode = CipherMode.CBC;
+            aesAlg.Padding = PaddingMode.PKCS7;
+
+            using (MemoryStream msDecrypt = new MemoryStream(cipherBytes, iv.Length, cipherBytes.Length - iv.Length))
+            using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, aesAlg.CreateDecryptor(), CryptoStreamMode.Read))
+            using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+            {
+                return srDecrypt.ReadToEnd();
+            }
         }
     }
 }
